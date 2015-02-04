5e6a089bffe45a8cd3b2f274771f9ca78d6423af
Rob Pickering <rob@icarus.bpoffice.ipcortex.net>
2015-02-04 09:27:09 +0000

    fix structure

--- change-origin/plugin.rb
+++
@@ -1,9 +0,0 @@
-# name: change-origin
-# about: changes Discourse origin policy
-# version: 0.1
-# authors: Rob Pickering
-
-register_asset "javascripts/change_origin.js"
-
-
-Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
